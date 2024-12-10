cb4accd5d738bdda9e9d25186eb6a9e637a68a0f
JyunSiang <62278191+JyunSiang84@users.noreply.github.com>
2024-12-10 14:42:18 +0800

    Create HelloWorld.java

---
+++ part1-basics/week1-environment/basics/program-structure/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 這是一個基本的 Java 程式範例
+ * 展示了 Java 程式的基本結構
+ */
+public class HelloWorld {
+    // main 方法是程式的進入點
+    public static void main(String[] args) {
+        // 印出 "Hello, World!" 到控制台
+        System.out.println("Hello, World!");
+        
+        // 示範基本變數使用
+        String message = "Welcome to Java!";
+        System.out.println(message);
+        
+        // 示範簡單的數值運算
+        int number = 42;
+        System.out.println("The number is: " + number);
+    }
+}
